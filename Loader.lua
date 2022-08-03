9f368879637e35ab9fc049723ff99b9aad0b5883
klinuxs <70549126+klinuxs@users.noreply.github.com>
2022-08-02 21:40:11 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,6 +3,6 @@ if game.PlaceId == 4836735146 then
 else
 	if game.PlaceId == 9884452497 then
 	else
-		
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/klinuxs/littlebitotrolling/main/BloodLust.lua'))()
 	end
 end
