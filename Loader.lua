943b88d766bdf592845e5cfee91827cfc2c854c4
klinuxs <70549126+klinuxs@users.noreply.github.com>
2022-08-02 21:35:20 -0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 4836735146 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/klinuxs/littlebitotrolling/main/StreetShootout.lua'))()
+else
+	if game.PlaceId == 9884452497 then
+	else
+		
+	end
+end
